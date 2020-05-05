d3b7937ded178a999becfe517459cf4308185f0e
LYF <liuyuanfeng1@stu.gdou.edu.cn>
2020-05-05 11:52:23 +0800

    5-5

---
+++ python/练习列表元组.py
@@ -0,0 +1,52 @@
+#列表
+
+list1=['physics','chemistry',"1997","2000"]
+list2=[1,2,3,4,5]
+list3=['a','b','c','d']
+
+print (list1[0]+str(list2[0]))
+list=[]
+list.append("Google")
+list.append("Runoob")
+print (list,len(list))
+del list[1]
+print (list,len(list))
+
+list1.reverse()
+print (list1)
+list1.sort()
+print (list1)
+
+vowels = ['e', 'a', 'u', 'o', 'i']
+# 降序
+vowels.sort(reverse=True)#反转输出
+# 输出结果
+print ('降序输出:', vowels)
+
+#元组
+tup1=('physics','chemistry',1997,2000)
+tup2=(1,2,3,4,5,6,7)
+print("tup1[0]:",tup1[0])
+print("tup2[1:5]:",tup2[1:5])
+del tup2
+try:
+    print (tup2)
+except:
+    print ("Erro")
+try:
+    tup1[0] = 100
+except:
+    print ("Erro2")
+
+#字典
+dict={'a':1,'b':2,'b':3}#key相同,代替前面的内容
+print (dict)
+dict['a']=2
+print(dict['a'])
+try:
+    del dict['a']
+    dict.clear() # 清空字典所有条目
+    del dict  
+    print (dict)
+except:
+    print ("Erro3")
