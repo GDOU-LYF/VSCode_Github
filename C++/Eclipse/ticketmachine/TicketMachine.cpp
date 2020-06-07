35616ea05ca62a745e029effa084c46de55d09a3
LYF <liuyuanfeng1@stu.gdou.edu.cn>
2020-06-07 16:06:11 +0800

    初始化

--- C++/Eclipse/ticketmachine/TicketMachine.cpp
+++ C++/Eclipse/ticketmachine/TicketMachine.cpp
@@ -11,7 +11,7 @@
 #include <iostream>
 using namespace std;
 
-TicketMachine::TicketMachine():PRICE(0) {
+TicketMachine::TicketMachine():PRICE(0),balance(0),total(0) {
 	// TODO Auto-generated constructor stub
 
 }
---
+++ C/c.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{ int x=0,y=1,z=0;
+if(x=z=y==1)
+x=3;
+printf("%d,%d\n",x,z);
+return 0;
+}
